5683db2c5715ebca2ae4f77ab967bd6ce8633b82
Tanim <88611927+from-tanim@users.noreply.github.com>
2024-10-09 18:58:18 +0600

    Update script.js

--- script.js
+++ script.js
@@ -57,6 +57,59 @@ let platforms = []; let sticks = [];
 function draw() {
 }
 
+ 
+let sceneOffset;
+const canvas = document.getElementById("game"); const ctx = canvas.getContext("2d");
+const canvasWidth = 375;
+const canvasHeight = 375;
+function draw() {
+ctx.save();
+ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+ctx.translate(-sceneOffset, 0);
+// Draw scene drawPlatforms(); drawHero();
+drawSticks();
+// Restore transformation ctx.restore();
+}
+
+
+ 
+const canvas = document.getElementById("game"); const ctx = canvas.getContext("2d");
+const canvasWidth = 375; const canvasHeight = 375; const platformHeight = 100;
+let platforms = [
+{x: 50, w: 50 },
+{x: 90, w: 30 },
+];
+function drawPlatforms() {
+platforms.forEach(({ x, w }) = {
+});
+// Draw platform
+ctx.fillStyle = "black";
+ctx.fillRect(x, canvasHeight - platformHeight, w, platformHeight);
+}
+
+ 
+const canvas = document.getElementById("game"); const ctx = canvas.getContext("2d");
+const canvasWidth = 375;
+const canvasHeight = 375;
+let sticks = [
+];
+{x: 100, length: 50, rotation: 90 }, {x: 120, length: 30, rotation: 45 },
+function drawSticks() {
+sticks.forEach((stick) ⇒ {
+ctx.save();
+// Move the anchor point to the start of the stick and rotate ctx.translate(stick.x, canvas Height - platformHeight); ctx.rotate((Math.PI / 180) * stick.rotation);
+// Draw stick ctx.beginPath();
+ctx.lineWidth
+= 2;
+ctx.moveTo(0, 0);
+ctx.lineTo(0, -stick.length); ctx.stroke();
+// Restore transformations ctx.restore();
+});
+}
+
+ 
+ 
+
   
 
   
